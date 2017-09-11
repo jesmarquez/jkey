d9bc2abd5f5a0276905f4ea8d95fde7ec266e94f
jesmarquez <jamarquez@uao.edu.co>
2017-09-11 09:29:58 -0500

    Agrega session para page index

--- pages/index.js
+++ pages/index.js
@@ -5,15 +5,26 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import Page from '../components/page'
 import Layout from '../components/layout'
 import CarHome from '../components/cardhome'
+import Session from '../util/session'
 
 export default class extends Page {
+
+  static async getInitialProps({req}) {
+    const session = new Session({req})
+
+    return {
+      session: await session.getSession(true),
+      userAgent: req ? req.headers['user-agent'] : navigator.userAgent
+    }
+  }
+
   render() {
     const muiTheme = getMuiTheme({
       userAgent: this.props.userAgent,
       });
     return (
       <MuiThemeProvider muiTheme={muiTheme}>
-        <Layout>
+        <Layout session={this.props.session}>
           <CarHome />
         </Layout>
       </MuiThemeProvider>
